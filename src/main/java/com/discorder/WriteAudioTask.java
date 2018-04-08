d61b93edc27cf3ff41193f3f13271090fe321ef5
ntm <admin@brdsa.org>
2018-04-07 22:00:35 -0500

    write audio task

--- src/main/java/com/discorder/WriteAudioTask.java
+++ src/main/java/com/discorder/WriteAudioTask.java
@@ -20,29 +20,56 @@ public class WriteAudioTask {
     private final static Logger logger = LoggerFactory.getLogger(WriteAudioTask.class);
     private final ConcurrentLinkedQueue<byte[]> encodePipeline;
     private final ScheduledExecutorService executor;
-    
+    private File audioFile;
+
     public WriteAudioTask(ConcurrentLinkedQueue<byte[]> pipeline) {
         this.encodePipeline = pipeline;
-        
+
         this.executor = Executors.newSingleThreadScheduledExecutor();
     }
-    
-    public void start() {
-        this.executor.scheduleAtFixedRate(this::run, 0, 20, TimeUnit.MILLISECONDS);
+
+    private boolean initFile(final String dirName) {
+        String fileName = new SimpleDateFormatter("yyyy-MM-dd - hh.mm.ss").format(new Date()) + ".mp3";
+        Path path = Paths.get(dirName);
+
+        if (Files.notExists(path)) {
+            logger.info("target file {} will be created", dirName);
+            this.audioFile = Files.createFile(Files.createDirectories(dirName), fileName).toFile();
+            return true;
+        }
+
+        return false;
     }
-    
+
+    public void start(String fileName) {
+        try {
+            this.initFile(fileName);
+            this.executor.scheduleAtFixedRate(this::run, 0, 20, TimeUnit.MILLISECONDS);
+        } except (IOException ioe) {
+            logger.error("unable to open audio file for writing", ioe);
+        }
+    }
+
     public void stop() {
         while (!this.encodePipeline.isEmpty()) {
             try {
-                this.executor.awaitTermination(30, TimeUnit.SECONDS);
+                this.executor.awaitTermination(5, TimeUnit.SECONDS);
             } catch (InterruptedException ex) {
                 logger.debug("interrupted waiting for executor to finish", ex);
             }
+        }
+
+        try {
+            this.audioFile.close();
+        } catch (IOException ioe) {
+            logger.error("got error during file close, whatever", ioe);
         }
     }
 
     private void run() {
         logger.debug("samples in encode queue: {}", this.encodePipeline.size());
-        this.encodePipeline.poll();
+        byte[] sample = this.encodePipeline.poll();
+
+        
     }
 }
